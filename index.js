db532bdd0aafcd0ff941803171cea2f5f4723fb7
Luis Carrion <lcarrionalvarez@gmail.com>
2021-09-15 17:54:24 -0500

    Configuring first route

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hola Mundo!-');
+const express = require('express')
+require('dotenv').config()
+
+//Crear el servidor de express
+const app = express();
+
+//Directorio publico
+app.use( express.static('public') )
+
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'))
+// TODO: Crud: Eventos
+
+
+// Escuchar peticiones
+
+app.listen( process.env.PORT, () => {
+  console.log(`Server corriendo en el puerto: ${process.env.PORT}`);
+})
